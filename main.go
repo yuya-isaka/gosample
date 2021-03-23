b5677c185893ddb5fddbde299c066aae2d02c346
zxwg6329@gmail.com <yuyaisaka87@gmail.com>
2021-03-23 22:15:02 +0900

    add main && remove sample

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
-package gosample
+package main
 
-func Sqrt(x flota64) float64{
-  z := 0.0
-  for i := 0; i < 1000; i++ {
-    z -= (z*z - x) / (2 * x)
-  }
-  return z
+import (
+	"fmt"
+)
+
+func Sqrt(x float64) float64 {
+	z := 0.0
+	for i := 0; i < 1000; i++ {
+		z -= (z*z - x) / (2 * x)
+	}
+	return z
+}
+
+func main() {
+	fmt.Println("Hello GO")
+	fmt.Println(Sqrt(4.0))
 }
